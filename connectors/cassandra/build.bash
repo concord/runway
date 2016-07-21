17473555753df62ad00f0a26c6cbf2b076727155
Rob Blafford <rob@concord.io>
2016-07-21 11:48:02 -0400

    Adding build script
    * Creating Dockerfile for packaging
    * Build script that packages everything
    * Fix typo
    * Update README

---
+++ connectors/cassandra/build.bash
@@ -0,0 +1,33 @@
+#!/bin/bash --login
+
+set -e
+CUR_DIR=$(pwd)
+GIT_ROOT=$(git rev-parse --show-toplevel)
+PROJ_ROOT="$GIT_ROOT/connectors/cassandra"
+BUILD_DIRECTORY="$PROJ_ROOT/build"
+META_DIRECTORY="$PROJ_ROOT/meta"
+THIRD_PARTY_DIRECTORY="$PROJ_ROOT/third_party"
+
+# Build deps
+if [[ ! -d "$THIRD_PARTY_DIRECTORY" ]]; then
+    cd "$META_DIRECTORY"
+    source source_ansible_bash
+    ansible-playbook -K playbooks/devbox_all.yml
+    deactivate
+fi
+
+# Build source
+if [[ ! -d "$BUILD_DIRECTORY" ]]; then
+    mkdir "$BUILD_DIRECTORY"
+fi
+cd "$BUILD_DIRECTORY"
+cmake ..
+NPROCS=$(grep -c ^processor /proc/cpuinfo)
+make -j$NPROCS
+    
+# Build Dockerfile
+cd $PROJ_ROOT
+sudo docker build -t runway/cassandra_sink:0.4.3 .
+echo "Don't forget to push: sudo docker push runway/cassandra_sink:0.4.3"
+cd $CUR_DIR
+    
